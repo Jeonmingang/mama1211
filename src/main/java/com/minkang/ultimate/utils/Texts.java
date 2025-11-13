18be2bd82e517e7947c790c4645129f962534991
SAMKOREA <123339750+Jeonmingang@users.noreply.github.com>
2025-11-13 19:10:44 +0900

    Delete src/main directory

--- src/main/java/com/minkang/ultimate/utils/Texts.java
+++
@@ -1,7 +0,0 @@
-
-package com.minkang.ultimate.utils;
-import org.bukkit.ChatColor;
-
-public class Texts {
-    public static String color(String s){ return ChatColor.translateAlternateColorCodes('&', s); }
-}
--- src/main/java/com/minkang/usp2/utils/Texts.java
+++
@@ -1,7 +0,0 @@
-
-package com.minkang.usp2.utils;
-import org.bukkit.ChatColor;
-
-public class Texts {
-    public static String color(String s){ return ChatColor.translateAlternateColorCodes('&', s); }
-}
